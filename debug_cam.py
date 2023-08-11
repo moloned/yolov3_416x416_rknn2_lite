47e654a1275c73ea31ac852334030fa998e9597c
David Moloney <moloned@gmail.com>
2023-08-11 11:27:16 +0100

    modified .gitignore & added debug_camera.py

--- debug_cam.py
+++ debug_cam.py
@@ -5,16 +5,25 @@ import cv2, datetime, time, os, sys
 from threading import Thread
 import numpy as np
 
-import cv2
+# cat /usr/local/bin/test_camera.sh
+#
+#if [[ -c /dev/video51 ]]; then
+#        gst-launch-1.0 v4l2src device=/dev/video33 io-mode=4 ! video/x-raw,format=NV12,width=720,height=576,framerate=15/1 ! xvimagesink > /dev/null 2>&1 &
+#        gst-launch-1.0 v4l2src device=/dev/video42 io-mode=4 ! video/x-raw,format=NV12,width=720,height=576,framerate=15/1 ! xvimagesink > /dev/null 2>&1 &
+#        gst-launch-1.0 v4l2src device=/dev/video51 io-mode=4 ! video/x-raw,format=NV12,width=720,height=576,framerate=15/1 ! xvimagesink > /dev/null 2>&1
+#elif [[ -c /dev/video31 ]]; then
+#        gst-launch-1.0 v4l2src device=/dev/video22 io-mode=4 ! video/x-raw,format=NV12,width=720,height=576,framerate=15/1 ! xvimagesink > /dev/null 2>&1 &
+#        gst-launch-1.0 v4l2src device=/dev/video31 io-mode=4 ! video/x-raw,format=NV12,width=720,height=576,framerate=15/1 ! xvimagesink > /dev/null 2>&1
+#elif [[ -c /dev/video11 ]]; then
+#        gst-launch-1.0 v4l2src device=/dev/video11 io-mode=4 ! video/x-raw,format=NV12,width=720,height=576,framerate=15/1 ! xvimagesink > /dev/null 2>&1
 
-cap = cv2.VideoCapture(11)
+video_dev = int(sys.argv[1])
+cap = cv2.VideoCapture(video_dev)
 
 while True:
-
     ret, frame = cap.read()
     cv2.imshow('MIPI cam0',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
 
 cap.release()
 cv2.destroyAllWindows()
